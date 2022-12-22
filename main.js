4a58476d0e5ff27fef7a9244b17c0edb42ca2b68
focxverr1 <117368249+focxverr1@users.noreply.github.com>
2022-12-22 17:05:14 +0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+function multiply(){
+    let num1 = document.getElementById('num-1').value;
+    let num2 = document.getElementById('num-2').value;
+    document.getElementById('result').innerHTML = num1 * num2;
+}
+
+function devide(){
+    let num1 = document.getElementById('num-1').value;
+    let num2 = document.getElementById('num-2').value;
+    document.getElementById('result').innerHTML = num1 / num2;
+    
+}
